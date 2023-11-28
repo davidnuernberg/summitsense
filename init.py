916b2be0fc0c708a725e3144cf5f19c0503bca36
David Nürnberg <145004092+davidnuernberg@users.noreply.github.com>
2023-11-28 08:56:50 +0100

    Delete init.py

--- init.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-
-from .meeting import Meeting
-from .transcription import Transcription 
-from .participants import Participants
-from .interface import Interface
-
-# Imports to make classes available
-# when importing summitsense
